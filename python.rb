269a89fab05d629adfdf0431d2dd15ea7cbcbfe6
ambidextrousTx <ravisinha@my.unt.edu>
2014-07-08 23:14:49 -0500

    Generate from template file

--- python.rb
+++ python.rb
@@ -3,13 +3,8 @@ class Python
   end
 
   def generate
-    puts 'import'
-    puts 'class my_class(object):'
-    puts '    def __init__(self):'
-    puts '        pass'
-    puts '    def another_method(self, args):'
-    puts '        pass'
-    puts 'if __name__ == "__main__":'
+    stub = File.open('python.txt').read
+    puts stub
   end
 end
 
