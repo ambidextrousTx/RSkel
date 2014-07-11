01c7a78cd4364310791692707fe877ee4bb1204a
ambidextrousTx <ravisinha@my.unt.edu>
2014-07-10 20:24:37 -0500

    Add basic CPP stub support

---
+++ cpp.rb
@@ -0,0 +1,10 @@
+class CPP
+  def initialize
+  end
+
+  def generate
+    stub = File.open('cpp.txt').read
+    puts stub
+  end
+end
+
--- ui.rb
+++ ui.rb
@@ -1,5 +1,6 @@
 require_relative 'python'
 require_relative 'java'
+require_relative 'cpp'
 
 language = gets.chomp.downcase
 supported = ['ruby', 'java', 'python', 'c++']
@@ -8,3 +9,4 @@ puts 'Supported' if supported.include? language
 
 Python.new.generate if language.eql? 'python'
 Java.new.generate if language.eql? 'java'
+CPP.new.generate if language.eql? 'c++'
