023e5e0e4c2f9ebe1d717f993c2484bc635fff9f
ambidextrousTx <ravisinha@my.unt.edu>
2014-07-14 22:22:28 -0500

    Support user specified class and method

--- java.rb
+++ java.rb
@@ -1,10 +1,22 @@
 class Java
-  def initialize
+  def initialize(classname, returntype, methodname)
+    @classname = classname.chomp
+    @returntype = returntype.chomp
+    @methodname = methodname.chomp
   end
 
   def generate
-    stub = File.open('java.txt').read
-    puts stub
+
+    puts
+    puts 'import *.*'
+    puts
+    puts "class #{@classname} {"
+    puts "  private #{@returntype} #{@methodname}(arguments) {"
+    puts '    // Do something'
+    puts '}'
+    puts '  public static void main(String[] args) {'
+    puts '    // Do something'
+    puts '}'
   end
 end
 
--- ui.rb
+++ ui.rb
@@ -6,9 +6,21 @@ require_relative 'ruby'
 language = gets.chomp.downcase
 supported = ['ruby', 'java', 'python', 'c++']
 
-puts 'Supported' if supported.include? language
+if supported.include? language
+  puts 'Supported' 
+  if language.eql? 'java'
+    puts 'Enter the name of the main class:'
+    classname = gets
+    puts 'Enter the return type of a method:'
+    returntype = gets
+    puts 'Enter the name of the method above:'
+    methodname = gets
+    java = Java.new(classname, returntype, methodname)
+    puts 'Here is the generated stub:'
+    java.generate
+  end
+end
 
 Python.new.generate if language.eql? 'python'
-Java.new.generate if language.eql? 'java'
 CPP.new.generate if language.eql? 'c++'
 Ruby.new.generate if language.eql? 'ruby'
